fa7637071d3569f9f026fad1e4a37a71222acc1a
aravind167-y <2311it010167@mallareddyuniversity.ac.in>
2026-09-23 14:44:59 +0530

    updated server code

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -1,8 +1,46 @@
-function getCourse(req,res){
-
+const Course = require('../models/course');
+async function getCourse(req,res){
+    try{
+        const courses = await Course.find();
+        return res.status(200).send(courses);
+    }catch(error){
+        return res.status(500).send({
+            message:"unable to fetch courses",
+        });
+    }
 }
-function createCourse(req,res){
-    
+async function createCourse(req,res){
+    try{
+        const { title, description, category, level, price, duration } = req.body;
+        if (!title || !description || !category || !level || !price || !duration) {
+            return res.status(400).send({ 
+                message: "Bad Request: Missing required fields" 
+            });
+        }
+        const existingCourse = await Course.findOne({ title:title });
+        if (existingCourse) {
+            return res.status(400).send({ 
+                message: "Conflict: Course with this title already exists" 
+            });
+        }
+        const course = new Course({
+            title: title,
+            description: description,
+            instructor: req.user._id,
+            category: category,
+            level: level,
+            price: price,
+            duration: duration
+        });
+        await course.save();
+        return res.status(200).send({
+            message: "Course created successfully",
+        });
+    }catch(error){
+        return res.status(500).send({
+            message:"unable to create course",
+        });
+    }
 }
 function deleteCourse(req,res){
     
