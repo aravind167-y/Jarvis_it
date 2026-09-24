07da9b226e4edaa47081e66de8ea701cb40a6008
aravind167-y <2311it010167@mallareddyuniversity.ac.in>
2026-09-24 11:43:40 +0530

    updated apis

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -42,14 +42,77 @@ async function createCourse(req,res){
         });
     }
 }
-function deleteCourse(req,res){
-    
+async function deleteCourse(req,res){
+    try{
+        const {id}=req.params;
+        const course = await Course.findById(id);
+        if(!course){
+            return res.status(400).send({
+                message:"Course not found",
+            });
+        }
+        if(req.user.role !== "instructor" && (!course.instructor || !course.instructor.equals(req.user._id))
+        ){
+            return res.status(403).send({
+                message:"You can only delete courses you created"
+            })
+        }
+        await course.deleteOne({_id:id})
+        return res.status(200).send({
+            message:"Course deleted"
+        })
+        
+    }catch(error){
+        return res.status(500).send({
+            message:"Unable to delete course"
+        })
+    }
 }
-function updateCourse(req,res){
-    
+async function updateCourse(req,res){
+    try{
+        const {id}=req.params;
+        const course = await Course.findById(id)
+        if(!course){
+            return res.status(404).send({
+                message:"Course not found"
+            })
+        }
+        const editableFields=[
+            "title",
+            "description",
+            "category",
+            "level",
+            "price",
+            "duration"
+        ]
+        editableFields.forEach((field)=>{
+            if (req.body[field] !== undefined){
+                course[field]=req.body[field]
+            }
+        })
+        await course.save()
+        return res.status(200).send({
+            message:"Course Updated"
+        })
+    } catch(error){
+        return res.status(500).send({
+            message:"Unable to update course"
+        })
+    }
 }
-function getCourseByID(req,res){
-    
+async function getCourseByID(req,res){
+    try{
+        const {id}=req.params;
+        const course = await Course.findById(id).populate('instructor', 'name email role');
+        if(!course){
+            return res.status(400).send({
+                message:"Course not found",
+            });
+        }
+        return res.status(200).send(course);
+    }catch(error){
+        console.log("hello");
+    }
 }
 module.exports={
     getCourse,
